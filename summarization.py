ea1e4e335c8ea811d7e493970b2fbc7b331421e4
Mahdi Parastesh <fulcrum1378@gmail.com>
2023-07-14 07:46:49 +0330

    2023.07.14 - 5

--- summarization.py
+++ summarization.py
@@ -6,9 +6,7 @@ from PIL import Image
 arr: np.ndarray = np.asarray(Image.open('vis/2/1689005849386887.bmp'))
 dim = 1080
 
-# Don't create another image, just alter the existing image.
 # WE DON'T NEED 90%+ OF THE DATA WE GET FROM THE CAMERA!
-
 # Humans' caring has a percentage that changes by the amount of stress they have!
 # We should later be able to tweak the "attention to details"!
 # We may even be able to avoid storing images! We just need a summary!
@@ -17,17 +15,12 @@ dim = 1080
 #   - SIMPLIFICATION
 #   - SUMMARIZATION  (I CHOOSE THIS, for the great extra performance)
 
-# Human beings SEE 30FPS, but do they UNDERSTAND 30FPS?
-# TODO I think we should lower the frame rate extensively!!
-# See?!? My Android phone can do a shit!!!
-
 # TODO
 
 plot.imshow(arr)
 plot.show()
 
-# After Simplification, we should recognise patterned objects too and finally group every object.
-# Then we will either ...
+# After Summarization, we will either ...
 #   - Store the objects separately.
 #   - Store the separate objects in ONE file.
 #   - Do no object separation; just store the vector!
