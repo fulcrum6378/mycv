a6f204deface4730be8974d5236a4635a3acd6ac
Mahdi Parastesh <fulcrum1378@gmail.com>
2023-07-21 00:19:03 +0330

    2023.07.20 - 5 (flexible iteration over images, incomplete because I don't know much about HSV integers)

--- summarization.py
+++ summarization.py
@@ -1,11 +1,15 @@
+from datetime import datetime
 import matplotlib.pyplot as plot
 import numpy as np
 from PIL import Image
 import sqlite3 as sql
 
+whole_time = datetime.now()
 # noinspection PyTypeChecker
-arr: np.ndarray = np.asarray(Image.open('vis/2/1689005849386887.bmp').convert('HSV'))
+arr: np.ndarray = np.asarray(Image.open('vis/2/1689005849386887.bmp').convert('HSV')).copy()
+arr.setflags(write=True)
 dim = 1088
+
 
 # Learning methods:
 # 1. Reinforcement Learning (organisms, slow, dangerous!)
@@ -25,5 +29,49 @@ dim = 1088
 # we could also store a simplified version of those images!
 # Forgetting can be accomplished by setting a last modified timestamp on each shape/vector/object.
 
+def is_hue_close(a: np.ndarray, b: np.ndarray) -> bool:
+    return abs(int(a[0]) - int(b[0])) <= 20
+
+
+status = np.repeat([np.repeat(False, dim)], dim, 0)
+sureY, sureX = 0, 0
+while sureY < dim:
+    print(sureY, sureX)
+    thisY, thisX = 0, 0
+    for y in range(sureY, dim):
+        break_it = False
+        for x in range(sureX, dim):
+            if not status[y, x]:
+                thisY = y
+                thisX = x
+                break_it = True
+                break
+            else:
+                sureY = y
+                sureX = x
+        if break_it: break
+
+    is_close: bool
+    for yy in range(thisY, dim):
+        for xx in range(thisX, dim):
+            status[yy, xx] = True
+            if yy == thisY and xx == thisX:
+                continue
+
+            if xx == thisX:
+                is_close = is_hue_close(arr[yy - 1, xx], arr[yy, xx])
+            else:
+                is_close = is_hue_close(arr[yy, xx - 1], arr[yy, xx])
+
+            if not is_close:
+                arr[yy, xx] = np.array([0, 255, 255])
+                break
+
+    sureX += 1
+    if sureX == dim:
+        sureX = 0
+        sureY += 1
+
 plot.imshow(Image.fromarray(arr, 'HSV').convert('RGB'))
+print(datetime.now() - whole_time)  # mere File->Image->RGB->HSV->RGB->Image->ImShow: 0:00:00.430~~480
 plot.show()
