f5ee94474287f4c8f030bf9fd37d7a3f0af1ca63
Mahdi Parastesh <fulcrum1378@gmail.com>
2023-08-24 19:35:15 +0330

    2023.08.24 - 2 (ready for actual clustering)

--- segmentation/clustering.py
+++ segmentation/clustering.py
@@ -9,6 +9,30 @@ arr: np.ndarray = np.asarray(Image.open('vis/2/1689005849386887.bmp').convert('H
 arr.setflags(write=True)
 dim = 1088
 
-plot.imshow(Image.fromarray(arr, 'HSV').convert('RGB'))
-print('Whole time:', datetime.now() - whole_time)  # mere File->Image->RGB->HSV->RGB->Image->ImShow: 0:00:00.430~~480
-plot.show()
+
+class Pixel:
+    def __init__(self, _c: np.ndarray, _y: int, _x: int):
+        self.c = _c
+        self.y = _y
+        self.x = _x
+
+
+pixels: list[Pixel] = list()
+for y in range(len(arr)):
+    for x in range(len(arr[y])):
+        pixels.append(Pixel(arr[y, x], y, x))
+
+
+def cluster(a: Pixel, b: Pixel) -> int:
+    pass
+
+
+pixels.sort(key=cluster)
+# pixels.sort(key=)  # we can also execute multiple sorting methods
+
+# print(pixels)
+# TODO show pixels in one dimension of mere colours
+
+# plot.imshow(Image.fromarray(arr, 'HSV').convert('RGB'))
+# print('Whole time:', datetime.now() - whole_time)
+# plot.show()
