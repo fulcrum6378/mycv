25efb4d9b820536387bc00dddbd54c624fdb578d
Mahdi Parastesh <fulcrum1378@gmail.com>
2023-09-18 18:26:19 +0330

    2023.09.18 - 9

--- storage/first.py
+++ storage/first.py
@@ -1,28 +1,26 @@
 import json
 import os
 
-import numpy as np
-
 
 class Shape:
-    def __init__(self, id_: str, y_: int, u_: int, v_: int, r_: int, ):
+    def __init__(self, id_: str, y_: int, u_: int, v_: int, r_: float):
         self.id: str = id_
         self.y: int = y_
         self.u: int = u_
         self.v: int = v_
-        self.r: int = r_  # ratio (width / height)
+        self.r: float = r_  # ratio (width / height)
 
 
+# load data
 output_dir, ext = os.path.join('tracing', 'output'), '.json'
-shapes: np.ndarray[Shape] = np.array([], dtype=Shape, ndmin=1)
+shapes: list[Shape] = []
 for o in os.listdir(output_dir):
     if not o.endswith(ext): continue
     seg = json.loads(open(os.path.join(output_dir, o), 'r').read())
     w, h = seg['dimensions']
-    # noinspection PyTypeChecker
-    shapes = np.append(shapes, Shape(o[:-len(ext)], *seg['mean'], w / h))
-print(sorted(shapes, key=lambda s: s.y))
-print("Shape of `shapes`:", shapes.shape)
+    shapes.append(Shape(o[:-len(ext)], *seg['mean'], w / h))
+
+shapes.sort(key=lambda s: s.u)
 for s1 in shapes:
     # for s2 in s1:
     print(s1.__dict__)
