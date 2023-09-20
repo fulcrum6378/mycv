674a475123a95e6859d23a5df0b029ec43e7f432
Mahdi Parastesh <fulcrum1378@gmail.com>
2023-09-21 00:08:55 +0330

    2023.09.21 - 1 (working on writing in a portion of a binary file...)

--- storage/feature_database.py
+++ storage/feature_database.py
@@ -2,66 +2,86 @@ import os
 import struct
 from typing import Optional
 
+with open('storage/output/test2', 'ab') as f:
+    f.seek(10)
+    f.write('THRAWN'.encode('ascii'))
+quit()
 
 class FeatureDB:
     ext = 'fdb'
 
     def __init__(self, name: str, key_length: int = 1):
-        self.path = os.path.join('storage', 'output', name + '.' + self.ext)
-        self.index_path = self.path + '-index'
-        self.key_length: int = key_length
-        self.key_type = {1: '>B', 2: '>H', 4: '>I', 8: '>Q'}[key_length]
-        self.define_structure()
-        self.index: dict[int, tuple[int, int]] = {}
+        self._path = os.path.join('storage', 'output', name + '.' + self.ext)
+        self._index_path = self._path + '-index'
+        self._key_length: int = key_length
+        self._key_type = {1: '>B', 2: '>H', 4: '>I', 8: '>Q'}[key_length]
+        self._define_structure()
+        self._index: dict[int, tuple[int, int]] = {}
 
-        if os.path.isfile(self.path):
-            if not os.path.isfile(self.index_path):
+        if os.path.isfile(self._path):
+            if not os.path.isfile(self._index_path):
                 raise Exception('Index file of the database is missing!')
-            with open(self.index_path, 'rb') as idx:
+            with open(self._index_path, 'rb') as idx:
                 raw_index = idx.read()
-            self.key_length = raw_index[0]
-            for b in range(1, len(raw_index), self.key_length + 16):
-                val2 = b + self.key_length
-                self.index[struct.unpack(self.key_type, raw_index[b:val2])[0]] = (
+            self._key_length = raw_index[0]
+            for b in range(1, len(raw_index), self._key_length + 16):
+                val2 = b + self._key_length
+                self._index[struct.unpack(self._key_type, raw_index[b:val2])[0]] = (
                     struct.unpack('>Q', raw_index[val2:val2 + 8])[0],
                     struct.unpack('>Q', raw_index[val2 + 8:val2 + 16])[0]
                 )
 
-    # noinspection PyAttributeOutsideInit
-    def define_structure(self):
-        self.data: dict[int, set[int]] = {}
+    def _define_structure(self):
+        self._data: dict[int, set[int]] = {}
 
     def read(self, key: int) -> Optional[set[int]]:
-        if key not in self.index: return None
-        with open(self.path, 'rb') as fdb:
-            fdb.seek(self.index[key][0])
-            data = fdb.read(self.index[key][1] * 8)
+        # if key not in self._index: return None  # could make it heavy!
+        with open(self._path, 'rb') as fdb:
+            fdb.seek(self._index[key][0])
+            data = fdb.read(self._index[key][1] * 8)
         ret = set()
         for b in range(0, len(data), 8):
             ret.add(struct.unpack('>Q', data[b:b + 8])[0])
         return ret
 
-    def write(self):
-        with open(self.path, 'wb') as fdb:
-            for k, ids in self.data.items():
-                offset = fdb.tell()
-                self.index[k] = (offset, len(ids))
-                for sid in ids: fdb.write(struct.pack('>Q', sid))
-        self.write_index()
+    def edit(self, key: int, value: set[int]) -> bool:
+        """ Returns True if calling save_index() is necessary. """
+        prev = None
+        this_idx = None
+        this_offset = None
+        for k in self._index:
+            if k < key:  # before this
+                prev = key
+                continue
+            elif k == key:  # this (which may not exist!)
+                this_idx = k
+                this_offset = self._index[k][0]
+                dif_items = len(value) - self._index[k][1]
+                if dif_items == 0: break
+            else:  # after this
+                if this_idx is None:
+                    this_idx = prev
+                    this_offset = self._index[k][0]
+                    dif_items = len(value)
+                else:
+                    self._index[k] = (self._index[k][0] + (dif_items * 8), self._index[k][1])
+        if this_idx is None:
+            this_offset = self._index[len(self._index)][0]
+            dif_items = len(value)
+        self._index[this_idx] = (this_offset, len(value))
+        with open(self._path, 'wb') as fdb:
+            fdb.seek(this_offset)
+            # TODO APPEND OR WRITE?!?!?
+            for sid in value: fdb.write(struct.pack('>Q', sid))
+        return dif_items != 0
 
-    def write_index(self):
-        with open(self.index_path, 'wb') as idx:
-            idx.write(struct.pack('>B', self.key_length))
-            for k, v in self.index.items():
-                idx.write(struct.pack(self.key_type, k))
+    def save_index(self):
+        with open(self._index_path, 'wb') as idx:
+            idx.write(struct.pack('>B', self._key_length))
+            for k, v in self._index.items():
+                idx.write(struct.pack(self._key_type, k))
                 idx.write(struct.pack('>Q', v[0]))
                 idx.write(struct.pack('>Q', v[1]))
-
-    def delete(self):
-        os.remove(self.path)
-        os.remove(self.index_path)
-
-# FIXME FUUUUUUUUUUCK self.data is empty; BECAUSE IT'S NEVER READ! WE SHOULD WRITE IN A WAY THAT...
 
 
 class FractionalFeatureDB(FeatureDB):
@@ -70,15 +90,14 @@ class FractionalFeatureDB(FeatureDB):
     def __init__(self, name: str, key_length: int = 2):
         super().__init__(name, key_length)
 
-    # noinspection PyAttributeOutsideInit
-    def define_structure(self):
-        self.data: dict[int, list[tuple[int, float]]] = {}
+    def _define_structure(self):
+        self._data: dict[int, list[tuple[int, float]]] = {}
 
     def read(self, key: int) -> Optional[list[tuple[int, float]]]:
-        if key not in self.index: return None
-        with open(self.path, 'rb') as fdb:
-            fdb.seek(self.index[key][0])
-            data = fdb.read(self.index[key][1] * 12)
+        if key not in self._index: return None
+        with open(self._path, 'rb') as fdb:
+            fdb.seek(self._index[key][0])
+            data = fdb.read(self._index[key][1] * 12)
         ret = list()
         for b in range(0, len(data), 12):
             ret.append((
@@ -87,33 +106,28 @@ class FractionalFeatureDB(FeatureDB):
             ))
         return ret
 
-    def write(self):
-        with open(self.path, 'wb') as fdb:
-            for k, ids_and_values in self.data.items():
+    def edit(self, key: int, value: list[tuple[int, float]]):
+        # FIXME
+        self._data[key].sort(key=lambda t: t[1])
+        with open(self._path, 'wb') as fdb:
+            for k, ids_and_values in self._data.items():
                 offset = fdb.tell()
-                self.index[k] = (offset, len(ids_and_values))
+                self._index[k] = (offset, len(ids_and_values))
                 for sid, val in ids_and_values:
                     fdb.write(struct.pack('>Q', sid))
                     fdb.write(struct.pack('>f', val))
-        self.write_index()
-
-    def sort(self, key: int):
-        """ DO THIS ON EVERY INSERT OR UPDATE! """
-        self.data[key].sort(key=lambda t: t[1])
 
 
 # write tests here.
 if __name__ == '__main__':
     my_fdb = FeatureDB('test', 2)
-    my_fdb.data = {1: {1001, 1002, 1003}, 2: {1004, 1005, 1006}}
-    my_fdb.write()
+    my_fdb.edit(1, {1001, 1002, 1003})
+    my_fdb.edit(2, {1004, 1005, 1006})
+    my_fdb.save_index()
     print(my_fdb.read(1))
 
     my_ffdb = FractionalFeatureDB('test', 2)
-    my_ffdb.data = {
-        1: [(1001, 1.1), (1002, 1.2), (1003, 1.3)],
-        2: [(1005, 1.5), (1004, 1.4), (1006, 1.6)],
-    }
-    my_ffdb.sort(2)
-    my_ffdb.write()
+    my_ffdb.edit(1, [(1001, 1.1), (1002, 1.2), (1003, 1.3)])
+    my_ffdb.edit(2, [(1005, 1.5), (1004, 1.4), (1006, 1.6)])
+    my_ffdb.save_index()
     print(my_ffdb.read(2))
