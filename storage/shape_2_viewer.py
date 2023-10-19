9cd52c587e25fef210748ec4ecc0c43cb6a83768
Mahdi Parastesh <fulcrum1378@gmail.com>
2023-10-19 11:47:29 +0330

    2023.10.19 - 1 (loop for shape_2_viewer)

--- storage/shape_2_viewer.py
+++ storage/shape_2_viewer.py
@@ -5,17 +5,21 @@ import numpy as np
 from config import shape_path_max
 from storage.sf2_global import *
 
-# load the subject shape
-sh = Shape(input('Enter the ID of the shape: '))
-print('Frame', sh.frame, ':', sh.m, sh.w, 'x', sh.h, '(', sh.r, ')')
-print(len(sh.path), 'points; centre:', sh.cx, 'x', sh.cy)
+while True:
+    # load the subject shape
+    try:
+        sh = Shape(input('Enter the ID of the shape: '))
+    except:
+        break
+    print('Frame', sh.frame, ':', sh.m, sh.w, 'x', sh.h, '(', sh.r, ')')
+    print(len(sh.path), 'points; centre:', sh.cx, 'x', sh.cy)
 
-# draw the segment into the cadre and display it
-sh.w *= 1.0 - (0.25 * (3 - shape_path_bytes))
-sh.h *= 1.0 - (0.25 * (3 - shape_path_bytes))
-arr = np.repeat(np.repeat(np.array([[sh.m]]), sh.w + 1, 1), sh.h + 1, 0)
-for p in sh.path:
-    arr[int(p[1] / (shape_path_max / sh.h)), int(p[0] / (shape_path_max / sh.w))] = \
-        0, 128, 128  # m[0], m[1], m[2]
-plot.imshow(cv2.cvtColor(np.array(arr, dtype=np.uint8), cv2.COLOR_YUV2RGB))
-plot.show()
+    # draw the segment into the cadre and display it
+    sh.w *= 1.0 - (0.25 * (3 - shape_path_bytes))
+    sh.h *= 1.0 - (0.25 * (3 - shape_path_bytes))
+    arr = np.repeat(np.repeat(np.array([[sh.m]]), sh.w + 1, 1), sh.h + 1, 0)
+    for p in sh.path:
+        arr[int(p[1] / (shape_path_max / sh.h)), int(p[0] / (shape_path_max / sh.w))] = \
+            0, 128, 128  # m[0], m[1], m[2]
+    plot.imshow(cv2.cvtColor(np.array(arr, dtype=np.uint8), cv2.COLOR_YUV2RGB))
+    plot.show()
